ae1f45d37134dda7c4524d0166eb6855ce4c6f34
djt <460391305@qq.com>
2018-11-26 21:56:14 +0800

    update list3

--- LeetCode-list-3/src/com/company/Main.java
+++ LeetCode-list-3/src/com/company/Main.java
@@ -24,6 +24,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        ListNode head = new ListNode(1);
+        ListNode head1 = new ListNode(2);
+        ListNode head2 = new ListNode(3);
+        ListNode head3 = new ListNode(4);
+        ListNode head4 = new ListNode(5);
+        head.next = head1;
+        head1.next = head2;
+        head2.next = head3;
+        head3.next = head4;
+        reverseList1(head);
     }
 
     /**
@@ -56,4 +66,26 @@ public class Main {
         }
         return rhead.next;
     }
+
+    /**
+     * 使用迭代方法
+     * */
+    public static ListNode reverseList1(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode res = new ListNode(0);
+        reverse(head, head.next, res);
+        head.next = null;
+        return res.next;
+    }
+
+    private static void reverse(ListNode pre, ListNode curr, ListNode res) {
+        if (curr == null) {
+            res.next = pre;
+            return;
+        }
+        reverse(pre.next, curr.next, res);
+        curr.next = pre;
+    }
+
 }
